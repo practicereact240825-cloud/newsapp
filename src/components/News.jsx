09d5354ce98b9dbbe927ef248fa707460a1dcd8c
practicegit080825 <practicegithub080825@gmail.com>
2025-08-27 14:17:30 +0530

    Complete-video-24: Fetch API Key from News API

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,19 +1,31 @@
-import React, { Component } from 'react'
-import NewsItem from './NewsItem'
+import React, { Component } from "react";
+import NewsItem from "./NewsItem";
 
 export class News extends Component {
-  static propTypes = {
-
-  }
-
+  static propTypes = {};
+  // 029c82a6864640fa85c3e517520ee25e
   render() {
     return (
-      <div>
+      <div className="container my-3">
+        <h1>NewsApp - Top Headlines</h1>
         This is a News Component.
-        <NewsItem />
+        <div className="row my-2">
+          <div className="col-md-3">
+            <NewsItem title="TTITLEEEEEEEE" description="DECRIPTIONNNNNN" />
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="TTITLEEEEEEEE" description="DECRIPTIONNNNNN" />
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="TTITLEEEEEEEE" description="DECRIPTIONNNNNN" />
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="TTITLEEEEEEEE" description="DECRIPTIONNNNNN" />
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default News
+export default News;
--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -2,10 +2,18 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
+    let {title, description} = this.props;
     return (
-      <div>
-        This is a News Item.
-      </div>
+      <>
+      <div className="card" style={{width: "18rem"}}>
+  <img src="https://gfx.nrk.no/7kT5zGoE2E-GnUKLojH1PQ0I1Nze4Ypu8lxM-oBjubQw.jpg" className="card-img-top" alt="..." />
+  <div className="card-body">
+    <h5 className="card-title">{title}</h5>
+    <p className="card-text">{description}</p>
+    <a href="#" className="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+      </>
     )
   }
 }
