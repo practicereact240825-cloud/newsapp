c25aa00a137b5dadda0fc64cbbf5a4f8538c5009
practicegit080825 <practicegithub080825@gmail.com>
2025-08-27 12:40:34 +0530

    Complete-video-22: Project 2 Setup + Introduction to Class based components - Note: Video-1-to-21 Were Related to Textutils, Function Based Component, Hosting and Improvement that I have Completed and Except Some Content! Now Video-22 onwards Class Based react Component and creation of an newsapp

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        Hello My First Class Based Component, FOR REAL!
+      </div>
+    )
+  }
+}
